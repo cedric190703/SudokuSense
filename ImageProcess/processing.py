8f07fe3bac401da54163df1d7917b860cf7df9a1
Cédric <cedric.brzyski@epita.fr>
2023-05-28 09:25:10 -0400

    finished processing and extracting digits  from the image v1.0

--- ImageProcess/processing.py
+++ ImageProcess/processing.py
@@ -11,12 +11,14 @@ def processing(image):
     blur = cv2.GaussianBlur(gray, (5,5), 0)
 
     # Apply the threshold on the blur image
-    binary = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
+    binary = cv2.adaptiveThreshold(blur, 255,
+                                cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 4)
     return binary
 
 def findGrid(binary):
     # Find the contours on the binary image
-    contours, _ = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(binary, 
+                                   cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Filter the contours based on their area
     contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 1000]
@@ -34,41 +36,105 @@ def findGrid(binary):
     # If no suitable contour is found, return None
     return None
 
+# Function to normalized the points
+# Needed to the perspective transform
+def normalizedP(contours):
+    # Create a new arrays of size (4 x 2)
+    rect = np.zeros((4, 2))
+
+    # Convert array in float32 type
+    rect = np.float32(rect)
+
+    # Get the sum of the contours
+    s = contours.sum(axis=1)
+
+    # Get the min and max value of the sum of the array
+    rect[0] = contours[np.argmin(s)]
+    rect[2] = contours[np.argmax(s)]
+
+    # Get the diff in the sum of the array
+    diff = np.diff(contours, axis=1)
+
+    # Get the min and max value of the diff of the sum of the original array
+    rect[1] = contours[np.argmin(diff)]
+    rect[3] = contours[np.argmax(diff)]
+    return rect
+
 # Function to get the perspective transform to facilitate the cutting of the grid after
 def perspectiveTransform(binary, contours):
     # Defines the desired size of the transformed grid in pixels
     grid_size = 400
 
     # Represents the desired points to the corners destination
-    destination_corners = np.float32([[0, 0], [grid_size, 0], [0, grid_size], [grid_size, grid_size]])
+    destination_corners = np.float32([[0, 0], [grid_size, 0],
+                                    [grid_size, grid_size], [0, grid_size]])
     
     # Ensure the source points are of type np.float32
     contours = np.float32(contours)
-
-    # Need to rotate the image if the grid have a angle not at 90 degrees
-    # TODO
+    
+    # Normalized the points for the perspective transform
+    normalized = normalizedP(contours)
 
     # Calculates the transformation matrix needed to perform the perspective transform
-    transformation_matrix = cv2.getPerspectiveTransform(contours, destination_corners)
-
+    transformation_matrix = cv2.getPerspectiveTransform(normalized, destination_corners)
+    
     # Apply the perspective transform on the binary image
-    transformed = cv2.warpPerspective(binary, transformation_matrix, (grid_size, grid_size))
+    transformed = cv2.warpPerspective(binary,
+                                    transformation_matrix, (grid_size, grid_size))
     return transformed
 
+# Function that extract the digits on the image
+# Cutting the image that have the perspective image
+def extractDigits(transformed):
+    # Create an array that contains all the digits images
+    digitImages = []
+
+    sized = 400//9
+
+    for i in range(9):
+        for j in range(9):
+            found = 0
+            x1, y1 = i * (sized), j * (sized)
+            x2, y2 = (i + 1) * sized, (j + 1) * sized
+            
+            # Get the contours on the image cropped
+            image = transformed[y1:y2, x1:x2]
+            contours, _ = cv2.findContours(image, cv2.RETR_LIST,
+                                        cv2.CHAIN_APPROX_SIMPLE)
+
+            # Clean and append each digit image
+            for contour in contours:
+                imX1, imY1, imX2, imY2 = cv2.boundingRect(contour)
+
+                # In case we have a digit contour detected
+                if(0.4 * sized < imY2 < 0.8 * sized and
+                    0.2 * sized <  imX2 < 0.8 * sized):
+                    # Clean the digit image
+                    digitImage = image[imY1:imY1 + imY2,
+                                        imX1:imX1 + imX2]
+                    digitImages.append((digitImage, i, j, 1))
+                    found = 1
+                    break
+
+            # Empty case
+            if(not found):
+                digitImages.append((image, i, j, 0))
+    return digitImages
+
 def mainProcessing(image_path):
+    digitImages = []
+
     # Load the image
     image = cv2.imread(image_path)
 
     # preprocess the image to get a binary image
     binary = processing(image)
 
-    cv2.imshow('Result', binary)
-    cv2.waitKey(0)
     # Try to find the Sudoku grid
     grid = findGrid(binary)
 
-    cv2.drawContours(image, [grid], -1, (0, 255, 0), 2)
-    cv2.imshow('Result2', image)
+    # To see the contous on the image
+    # cv2.drawContours(image, [grid], -1, (0, 255, 0), 2)
     
     if(grid is not None):
         try:
@@ -78,17 +144,31 @@ def mainProcessing(image_path):
             # Apply the perspective transform to the binary image
             transformed = perspectiveTransform(binary, reshaped_grid)
 
+            # Invert the pixels on the image to have better digits on the image
+            # White, Black pixels  -> Black, White pixels
+            inverted = cv2.bitwise_not(transformed)
+
             # Display the transformed image
-            cv2.imshow('Transformed Image', transformed)
-            cv2.waitKey(0)
-        except:
-            cv2.destroyAllWindows()
-            return 0
+            # cv2.imshow('Transformed Image', inverted)
+
+            #  Get the list of all the digiit cases
+            digitImages = extractDigits(inverted)
+
+            # Tests to check the number of digits recognized           
+            """cpt = 0
+            for digit in digitImages:
+                if(digit[3]):
+                    cpt+=1
+            print(cpt)"""
+
+        except Exception as e:
+            # cv2.destroyAllWindows()
+            return (0, digitImages)
 
     # Destroy all the windows to liberate memory allocation
-    cv2.destroyAllWindows()
-    return 1
+    # cv2.destroyAllWindows()
+    return (1, digitImages)
 
 # Test the function here :
-image_path = 'C:\\Users\\cbrzy\\OneDrive\\Bureau\\R.jpg'
-print(mainProcessing(image_path))
+"""image_path = 'PATH HERE'
+mainProcessing(image_path)"""
