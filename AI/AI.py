8d0b31c15511817526931b1aa46c6331e4efd475
Cédric <cedric.brzyski@epita.fr>
2023-06-02 17:04:06 +0200

    Update main README and change the AI CNN model

--- AI/AI.py
+++ AI/AI.py
@@ -1,48 +1,66 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Keras libraries for datasets and CNN's model
 from tensorflow.keras.datasets import mnist
 
-# Main function for the AI part
-def mainAI():
+def main_ai():
+    """Create and save a CNN model"""
     # Load data from the mnist dataset into 2 main catergories :
     # Training data and Testing data
     # Handwritten digits dataset
-    (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
     # No need to clean this dataset
-
+    
     # Preprocess the data
-    xTrain = xTrain.reshape((60000, 28, 28, 1)) / 255.0
-    xTest = xTest.reshape((10000, 28, 28, 1)) / 255.0
-    yTrain = tf.keras.utils.to_categorical(yTrain, num_classes=10)
-    yTest = tf.keras.utils.to_categorical(yTest, num_classes=10)
+    input_shape = (28, 28, 1)
+
+    # Normalization and reshaping
+    x_train=x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+    x_train=x_train / 255.0
+    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+    x_test=x_test/255.0
+
+    # Encode labels
+    y_train = tf.one_hot(y_train.astype(np.int32), depth=10)
+    y_test = tf.one_hot(y_test.astype(np.int32), depth=10)
+
+    batch_size = 64
+    num_classes = 10
+    epochs = 6
+
+    # Stop if we have a good accuracy
+    class myCallback(tf.keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, logs={}):
+            if(logs.get('accuracy')>0.9965):
+                self.model.stop_training = True
+
+    callbacks = myCallback()
 
     # Create the CNN model
-    model = tf.keras.Sequential([
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-        tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
-        tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
-        tf.keras.layers.MaxPooling2D(),
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=input_shape),
+        tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu'),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
+        tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPool2D(strides=(2,2)),
+        tf.keras.layers.Dropout(0.25),
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(10, activation='sigmoid')
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
     ])
-    
-    # Compile the model
+
     model.compile(optimizer='adam',
-    loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy'])
-
-    model.summary()
-
+    loss='categorical_crossentropy', metrics=['accuracy'])
+    
     # Train the model
-    trained = model.fit(xTrain, yTrain, epochs=10,
-    validation_data=(xTest, yTest))
+    trained = model.fit(x_train, y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    validation_data=(x_test, y_test),
+    callbacks=[callbacks])
     
     # Get the plot from the data to show the accuracy of the model
     # Plot for the accuracy of the model
@@ -66,11 +84,10 @@ def mainAI():
     plt.close()
 
     # Calculates the accuracy of the model on the dataset
-    test_loss, test_acc = model.evaluate(xTest,  yTest, verbose=2)
+    test_loss, test_acc = model.evaluate(x_test, y_test)
     
-    # Save the parameters only if the accuracy is greater than 98.6%
-    if(test_acc * 100 > 98.6):
-        model.save('model.h5')
-    print(test_acc)
+    # Save the parameters of the model
+    model.save('model.h5')
 
-mainAI()
+# To get the 
+main_ai()
